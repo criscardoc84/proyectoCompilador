2e37e7f16b6a01cdd7238570fc5ba554345bb0bb
Cristian Cardona C <cristian.cardoc84@gmail.com>
2021-11-15 15:34:55 -0500

    Fin
    Se entrego el proyecto

--- compiler/src/algo.py
+++ compiler/src/algo.py
@@ -56,44 +56,44 @@ def t_error(t):
   print(" Caracter Invalido'%s'" % t.value[0])
   t.lexer.skip(1)
   
-#def buscarFicheros(directorio):
-#  ficheros = []
-#  numArchivo = ''
-#  respuesta = False
-#  cont = 1
-  
-#  for base, dirs, files in os.walk(directorio):
-#    ficheros.append(files)
-    
-#  for file in files:
-#    print(str(cont)+". "+file)
-#    cont= cont+1
-    
-#  while respuesta == False:
-#    numArchivo = input('\nNumero del test: ')
-#    for file in files:
-#      if file == files[int(numArchivo)-1]:
-#        respuesta = True
-#        break
+def buscarFicheros(directorio):
+  ficheros = []
+  numArchivo = ''
+  respuesta = False
+  cont = 1
 
-#  print("Se escogio \"%s\" \n" %files[int(numArchivo)-1])
-  
-#  return files[int(numArchivo)-1]
+  for base, dirs, files in os.walk(directorio):
+    ficheros.append(files)
+    
+  for file in files:
+    print(str(cont)+". "+file)
+    cont= cont+1
+    
+  while respuesta == False:
+    numArchivo = input('\nNumero del test: ')
+    for file in files:
+      if file == files[int(numArchivo)-1]:
+        respuesta = True
+        break
+
+  print("Se escogio \"%s\" \n" %files[int(numArchivo)-1])
+ 
+  return files[int(numArchivo)-1]
 
 
 #Esta linea va a variar de acuerdo a donde gaurdas el archivo.
-#directorio = '/Users/crist/Documents/GitHub/proyectoCompilador/compiler/test/'
-#archivo = buscarFicheros(directorio)
-#test = directorio+archivo
-#fp = codecs.open(test,"r","utf-8")
-#cadena = fp.read()
-#fp.close()
+directorio = '/Users/crist/Documents/GitHub/proyectoCompilador/compiler/test/'
+archivo = buscarFicheros(directorio)
+test = directorio+archivo
+fp = codecs.open(test,"r","utf-8")
+cadena = fp.read()
+fp.close()
 
 analizador = lex.lex()
 
-#analizador.input(cadena)
+analizador.input(cadena)
 
-#while True:
-#  tok = analizador.token()
-#  if not tok : break
-#  print(tok)
+while True:
+  tok = analizador.token()
+  if not tok : break
+  print(tok)
